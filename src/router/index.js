8fa3f1998e58981153f2624f9464755c30707f88
Asher Lien <birdym.tw@gmail.com>
2023-08-12 10:46:14 +0800

    首頁:
    Header完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import layout from '../layouts/default.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import layout from '../layouts/default.vue';
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -9,40 +9,46 @@ const router = createRouter({
       path: '/',
       name: 'deafult',
       component: () => import('../layouts/default.vue'),
-      children:[
+      children: [
         {
-          path:'home',
-          name:'HomeView',
-          component: () => import('../views/HomeView.vue')
+          path: 'home',
+          name: 'HomeView',
+          component: () => import('../views/HomeView.vue'),
         },
         {
-          path:'information',
-          name:'InformationView',
-          component: () => import('../views/informationView.vue')
+          path: 'information',
+          name: 'InformationView',
+          component: () => import('../views/informationView.vue'),
         },
         {
-          path:'facility',
-          name:'FacilityView',
-          component: () => import('../views/FacilityView.vue')
+          path: 'facility',
+          name: 'FacilityView',
+          component: () => import('../views/FacilityView.vue'),
         },
         {
-          path:'news',
-          name:'NewsView',
-          component: () => import('../views/NewsView.vue')
+          path: 'news',
+          name: 'NewsView',
+          component: () => import('../views/NewsView.vue'),
         },
         {
-          path:'login',
-          name:'LoginView',
-          component: () => import('../views/LoginView.vue')
+          path: 'login',
+          name: 'LoginView',
+          component: () => import('../views/LoginView.vue'),
         },
         {
-          path:'register',
-          name:'RegisterView',
-          component: () => import('../views/RegisterView.vue')
+          path: 'register',
+          name: 'RegisterView',
+          component: () => import('../views/RegisterView.vue'),
         },
-      ]
+      ],
     },
-  ]
-})
+    // 最一開始的頁面
+    {
+      path: '/index',
+      name: 'index',
+      component: () => import('../views/index.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;

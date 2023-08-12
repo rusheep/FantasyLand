fc86e76ad6c00dcb6b7641b6171c9425adbf0f66
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2023-08-12 11:07:43 +0800

    Header html css (#2)
    * 首頁:
    
    Header完成
    
    * 更新

--- src/components/-Global/Header.vue
+++ src/components/-Global/Header.vue
@@ -1,10 +1,52 @@
 <template>
-      <nav>
-        <RouterLink to="/home">樂園首頁</RouterLink>
-        <RouterLink to="/information">門票資訊</RouterLink>
-        <RouterLink to="/facility">設施狀態</RouterLink>
-        <RouterLink to="/news">最新消息</RouterLink>
-        <RouterLink to="/login">登入</RouterLink>
-        <RouterLink to="/register">註冊</RouterLink>
-      </nav>
-</template>>
+  <div class="header-container">
+    <img src="@/../public/logo.png" class="logo">
+    <header>
+      <h1>夢幻尼樂園</h1>
+    </header>
+    <nav>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss">
+.header-container {
+  position: relative;
+  top: -35px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
+  img {
+    width: 80px;
+
+  }
+
+  .logo {
+    position: relative;
+    top: 40px;
+    z-index: 1;
+  }
+
+  header {
+    width: 80%;
+    height: 70px;
+    background-color: #30B0C9;
+    position: relative;
+    z-index: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+
+    h1 {
+      margin-top: 30px;
+      font-size: 18px;
+      font-weight: bold;
+      color: white;
+
+    }
+  }
+}
+</style>

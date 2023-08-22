40b992983904a0276f0b3830481205283dc74f3b
YO-XUAN <yo0983987618@gmail.com>
2023-08-22 15:45:37 +0800

    串接完成

--- src/views/touristBackStage/touristTicket.vue
+++ src/views/touristBackStage/touristTicket.vue
@@ -1,9 +1,7 @@
 <script setup>
 import axios from 'axios';
 import { reactive } from 'vue';
-import TicketAdult from '../../components/Ticket/TicketAdult.vue';
-import TicketKid from '../../components/Ticket/TicketKid.vue';
-import TicketPrivileged from '../../components/Ticket/TicketPrivileged.vue';
+import { getFormatDateToISOString } from '../../composable';
 
 // 彈窗開關
 const infoModal = ref(false);
@@ -13,6 +11,15 @@ const selectedDate = ref('');
 
 // 總價
 const totalPrice = ref(0);
+
+//狀態
+const status = ref('');
+
+//快速通關
+const fastTrack = ref('');
+
+//票種
+const ticketType = ref('');
 
 //目前有買的票
 const userTickets = ref([]); // 將 userTickets 初始值改為一個空陣列
@@ -26,11 +33,33 @@ onMounted(async () => {
     const unseOrTodayUsedTicket = userTicketsAPI.data;
     userTickets.value = unseOrTodayUsedTicket;
     console.log (userTickets.value);
-
+    // 換算票的時間
+    const ticketDate = unseOrTodayUsedTicket[0]?.ticketDate;
+    const formattedDate = getFormatDateToISOString(ticketDate);
+    unseOrTodayUsedTicket.forEach(ticket => {
+      // 將元素的值設定到相應的響應式變數中
+      selectedDate.value = formattedDate;
+      status.value = ticket.status;
+      fastTrack.value = ticket.ticketCategoryId.fastTrack;
+      ticketType.value = ticket.ticketCategoryId.ticketType;
+    });
   } catch (error) {
     console.error(error);
   }
 });
+
+//判斷票券顏色
+
+const getColorByTicketType = (ticketType) => {
+  if (ticketType === '成人票') {
+    return '#00B9D2';
+  } else if (ticketType === '兒童票') {
+    return '#4786D0';
+  } else if (ticketType === '優待票') {
+    return '#04D200';
+  }
+  return '#FFFFFF'; // 默認顏色
+};
 
 
 
@@ -45,16 +74,22 @@ onMounted(async () => {
   <main>
     <div class="title">
       <h2 class="tickstatus">未使用</h2>
-      <h2 class="date">2023/05/23</h2>
+      <h2 class="date">{{ selectedDate }}</h2>
     </div>
     <section class="top-box"> 
-      <div class="card" v-for="ticket in userTickets" :key="ticket._id">
+      <div class="card" 
+        v-for="ticket in userTickets" :key="ticket._id" 
+        :style="{ 'backgroundColor': status === 'unuse' ? getColorByTicketType(ticket.ticketCategoryId.ticketType) : '#FFFFFF' }">
   <!-- 在這裡放置你想顯示的票券資訊 -->
-          {{ ticket._id }}
-          {{ ticket.purchaseDate }}
-          {{ ticket.status }}
-          {{ ticket.ticketCategoryId.ticketType }}
-          {{ ticket.ticketCategoryId.fastTrack }}
+        <div class="box-content">
+          <div class="status" 
+            :style="{ 'display': status === 'unuse' ? 'none' : 'block' }">
+            <p>{{ status === 'used' ? '已使用' : ''  }}</p>
+          </div>
+          <div class="ticketitle">{{ ticket.ticketCategoryId.ticketType }}</div>
+          <h3>{{ ticket.ticketCategoryId.fastTrack ? '快速通關' : '普通票' }}</h3>
+        </div>
+        <img src="@/../public/QRcode.png" class="QRcode">
       </div>
     </section>
     <div class="title">
@@ -71,23 +106,11 @@ onMounted(async () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>2023-08-11</td>
-            <td>已使用</td>
-            <td>成人票（快速通關）</td>
-            <td>200元</td>
-          </tr>
-          <tr>
-            <td>2023-08-11</td>
-            <td>已使用</td>
-            <td>成人票（快速通關）</td>
-            <td>200元</td>
-          </tr>
-          <tr>
-            <td>2023-08-11</td>
-            <td>已使用</td>
-            <td>成人票（快速通關）</td>
-            <td>200元</td>
+          <tr v-for="ticket in userTickets" :key="ticket._id">
+            <td>{{ selectedDate }}</td>
+            <td>{{ ticket.status === 'used' ? '已使用' : '未使用' }}</td>
+            <td>{{ ticket.ticketCategoryId.ticketType }}</td>
+            <td>{{ ticket.currentPurchasePrice }}元</td>
           </tr>
         </tbody>
       </table>
@@ -133,9 +156,42 @@ main {
 
   .top-box {
     .card {
-      width: 250px;
-      border: 1px solid black;
+    display: flex;
+    justify-content: space-around;
+    width: 26%;
+    background-color: #30B0C9;;
+    border-radius: 10px;
+    padding:1rem;
+    box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.2),
+              0px 8px 20px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    .box-content {
+      font-size: 25px;
+      font-weight: bold;
+      color: #fff;
+      display: flex;
+      flex-direction: column;
+      justify-content: end;
+      padding-bottom: 1rem;
+
+      .status {
+        p {
+          font-size: 12px;
+        }
+        padding: 5px;
+        width: 3rem;
+        text-align: center;
+        border: 1px solid #fff;
+        border-radius: 5px;
+        margin-bottom: 0.5rem;
+        /* display: none; */
+      }
+
+      .ticketitle {
+        padding-bottom: 1rem;
+      }
     }
+  }
   }
 
   .btm-box {

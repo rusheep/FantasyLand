fc45809acae9e987d12b158b1a46fc43a32c7f7f
Asher Lien <birdym.tw@gmail.com>
2023-08-22 14:28:54 +0800

    完成

--- src/views/cms/AuthView.vue
+++ src/views/cms/AuthView.vue
@@ -1,7 +1,22 @@
 <template>
   <main>
-    <!-- <QrStream @decode="handleQrCodeScan" /> -->
+    <QrStream @decode="handleQrCodeScan" />
     <div v-if="scannedUrl">Scanned URL: {{ scannedUrl }}</div>
+
+    <h3>驗票紀錄</h3>
+    <ul class="ticket-list">
+      <li
+        v-for="(ticket, index) in ticketList"
+        :key="index"
+        class="ticket-item"
+      >
+        <div class="text">
+          <strong>名稱:</strong> {{ ticket.userId.name }}<br />
+          <strong>入園時間:</strong> {{ ticket.date }}<br />
+          <strong>票型:</strong> {{ ticket.ticketCategoryId }}<br />
+        </div>
+      </li>
+    </ul>
   </main>
 </template>
 
@@ -28,12 +43,41 @@ const handleQrCodeScan = async (result) => {
     }
   }
 };
+
+const ticketList = ref([
+  // Add more ticket objects as needed
+]);
+
+onMounted(() => {
+  try {
+    const api = axios.get('/api/v1/authTicket').then((res) => {
+      ticketList.value = res.data;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 </script>
 
 <style lang="scss">
-//
 main {
   margin: 0 auto;
   width: 75%;
+
+  .ticket-list {
+    list-style: none;
+    padding: 0;
+
+    .ticket-item {
+      border: 1px solid #ccc;
+      padding: 15px;
+      margin-bottom: 10px;
+      background-color: #f5f5f5;
+
+      strong {
+        font-weight: bold;
+      }
+    }
+  }
 }
 </style>

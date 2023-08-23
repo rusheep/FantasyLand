2e94b2fa74699d64f4719cee27c27df23918816c
Asher Lien <birdym.tw@gmail.com>
2023-08-23 19:51:47 +0800

    完成

--- src/router/index.js
+++ src/router/index.js
@@ -84,6 +84,16 @@ const router = createRouter({
         },
       ],
     },
+    {
+      path: '/404',
+      name: '404',
+      component: () => import('../views/404.vue'),
+    },
+
+    {
+      path: '/:pathMatch(.*)',
+      redirect: '/404',
+    },
   ],
 });
 

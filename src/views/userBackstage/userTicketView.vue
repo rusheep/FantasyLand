9f98fee7944eeb4c51c101bceb1a76b5fa28b2e7
Asher Lien <birdym.tw@gmail.com>
2023-09-12 13:58:25 +0800

    票券 動畫

--- src/views/userBackstage/userTicketView.vue
+++ src/views/userBackstage/userTicketView.vue
@@ -62,14 +62,16 @@ function toCart() {
         <h3>{{ selectedDate }}</h3>
       </div>
       <!-- 單個票券 -->
-      <div class="ticketBox">
+
+      <transition-group name="list" appear tag="div" class="ticketBox">
         <Tickets v-for="(ticket, index) in userTickets" :key="ticket._id" :ticketData="ticket"
           @click="openRefundModal(ticket)" />
-        <div class="noTicketBox" v-if="ticketBoxToggele">
-          <h2>目前沒有訂票</h2>
-          <Button @click="toCart">前往訂票頁面</Button>
-        </div>
+      </transition-group>
+      <div class="noTicketBox" v-if="ticketBoxToggele">
+        <h2>目前沒有訂票</h2>
+        <Button @click="toCart">前往訂票頁面</Button>
       </div>
+
 
       <div class="title">
         <h2>使用紀錄</h2>
@@ -137,5 +139,46 @@ main {
     text-align: center;
     padding: 50px;
   }
+}
+
+.list-enter-from {
+  opacity: 0;
+  transform: scale(0.5);
+  /* Start from a smaller size */
+  transform-origin: center center;
+  /* Scale from the center */
+}
+
+.list-enter-to {
+  opacity: 1;
+  transform: scale(1);
+  /* Scale to the normal size */
+}
+
+.list-enter-active {
+  transition: all 0.4s ease;
+}
+
+.list-leave-from {
+  opacity: 1;
+  transform: scale(1);
+  /* Start from the normal size */
+  transform-origin: center center;
+  /* Scale from the center */
+}
+
+.list-leave-to {
+  opacity: 0;
+  transform: scale(0.5);
+  /* Scale down to a smaller size */
+}
+
+.list-leave-active {
+  transition: all 0.4s ease;
+  position: absolute;
+}
+
+.list-move {
+  transition: transform 0.4s ease, opacity 0.1s ease;
 }
 </style>

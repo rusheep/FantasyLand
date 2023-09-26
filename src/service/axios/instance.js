972dec1842482e7494fc2212f78a2266c439f2d8
Asher Lien <asher.lien@riversoft.com.tw>
2023-09-26 14:57:27 +0800

    初步建立檔案

---
+++ src/service/axios/instance.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+const defaultConfig = {
+  timeout: import.meta.env.VITE_SERVICE_TIMEOUT,
+  baseURL: import.meta.env.VITE_URL,
+};
+
+const axiosInstance = axios.create(defaultConfig);
+
+export default class Http {
+  constructor() {
+    this.axiosInstance = axios.create(defaultConfig);
+    this.httpInterceptorRequest();
+    this.httpInterceptorResponse();
+  }
+
+  httpInterceptorRequest() {
+    this.axiosInstance.interceptors.request.use(
+      (config) => {
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  httpInterceptorResponse() {
+    this.axiosInstance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
+  }
+
+  async get(url, params) {
+    const response = await this.axiosInstance.get(url, { params });
+    return response.data;
+  }
+
+  async post(url, data) {
+    const response = await this.axiosInstance.post(url, data);
+    return response.data;
+  }
+
+  async patch(url, data) {
+    const response = await this.axiosInstance.patch(url, data);
+    return response.data;
+  }
+
+  async delete(url, data) {
+    const response = await this.axiosInstance.delete(url, data);
+    return response.data;
+  }
+}
